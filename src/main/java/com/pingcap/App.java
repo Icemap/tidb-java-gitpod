09156e26d8a4c035b8c6fa0b2e10627aef73a519
Icemap <qizhi.wang@pingcap.com>
2022-06-01 15:50:51 +0800

    [Update] completed

--- src/main/java/com/pingcap/App.java
+++ src/main/java/com/pingcap/App.java
@@ -1,13 +1,25 @@
 package com.pingcap;
 
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws SQLException {
+        MysqlDataSource mysqlDataSource = new MysqlDataSource();
+        mysqlDataSource.setURL("jdbc:mysql://localhost:4000/test?user=root");
+        try (Connection connection = mysqlDataSource.getConnection()) {
+            ResultSet rs = connection.createStatement().executeQuery("SELECT VERSION()");
+            while (rs.next()) {
+                System.out.println(rs.getString(1));
+            }
+        }
     }
 }
