47cc871b6c996c08d954df224affd6036d6d74be
Icemap <cheese.javaee@gmail.com>
2022-06-12 16:36:52 +0800

    [Update] TiDB not support session state change

--- src/main/java/com/pingcap/App.java
+++ src/main/java/com/pingcap/App.java
@@ -1,18 +1,14 @@
 package com.pingcap;
 
 import com.mysql.cj.MysqlConnection;
-import com.mysql.cj.Query;
 import com.mysql.cj.conf.PropertyDefinitions;
 import com.mysql.cj.conf.PropertyKey;
-import com.mysql.cj.interceptors.QueryInterceptor;
-import com.mysql.cj.jdbc.MysqlDataSource;
-import com.mysql.cj.log.Log;
-import com.mysql.cj.protocol.Resultset;
-import com.mysql.cj.protocol.ServerSession;
+import com.mysql.cj.protocol.ServerSessionStateController;
 
 import java.sql.*;
 import java.util.Properties;
-import java.util.function.Supplier;
+
+import static com.mysql.cj.protocol.ServerSessionStateController.*;
 
 /**
  * Hello world!
@@ -25,45 +21,29 @@ public class App
         Properties props = new Properties();
         props.setProperty(PropertyKey.sslMode.getKeyName(), PropertyDefinitions.SslMode.DISABLED.name());
         props.setProperty(PropertyKey.allowPublicKeyRetrieval.getKeyName(), "true");
-        Connection preInsertConn = DriverManager.getConnection(dbUrl, props);
-        preInsertConn.createStatement().execute("DROP TABLE IF EXISTS testReversalOfScanFlags");
-        preInsertConn.createStatement().execute("CREATE TABLE testReversalOfScanFlags (f1 INT, f2 VARCHAR(255))");
-        preInsertConn.createStatement().execute("INSERT INTO testReversalOfScanFlags VALUES (1,\"a\"),(2,\"b\"),(3, \"c\")");
+        props.setProperty(PropertyKey.trackSessionState.getKeyName(), "true");
+        props.setProperty(PropertyKey.characterEncoding.getKeyName(), "latin1");
+        Connection stateChangeConn = DriverManager.getConnection(dbUrl, props);
 
-        props.setProperty(PropertyKey.queryInterceptors.getKeyName(), IndexQueryInterceptor.class.getName());
-        Connection scanningConn = DriverManager.getConnection(dbUrl, props);
-        scanningConn.createStatement().executeQuery("SELECT f2 FROM testReversalOfScanFlags");
-    }
+        ((MysqlConnection) stateChangeConn).getServerSessionStateController()
+                .addSessionStateChangesListener(stateChange -> {
+                    System.out.println("State change effect");
+                    for (ServerSessionStateController.SessionStateChange change : stateChange.getSessionStateChangesList()) {
+                        switch (change.getType()) {
+                            case SESSION_TRACK_SYSTEM_VARIABLES:
+                                System.out.println("State change type is SESSION_TRACK_SYSTEM_VARIABLES");
+                                break;
+                            case SESSION_TRACK_SCHEMA:
+                                System.out.println("State change type is SESSION_TRACK_SCHEMA");
+                                break;
+                            case SESSION_TRACK_STATE_CHANGE:
+                                System.out.println("State change type is SESSION_TRACK_STATE_CHANGE");
+                                break;
+                        }
+                    }
+                });
 
-    public static class IndexQueryInterceptor implements QueryInterceptor {
-
-        public QueryInterceptor init(MysqlConnection conn, Properties props, Log log) {
-            return this;
-        }
-
-        public <T extends Resultset> T preProcess(Supplier<String> sql, Query interceptedQuery) {
-            return null;
-        }
-
-        public boolean executeTopLevelOnly() {
-            return false;
-        }
-
-        public void destroy() {
-        }
-
-        public <T extends Resultset> T postProcess(Supplier<String> sql, Query interceptedQuery, T originalResultSet, ServerSession serverSession) {
-            System.out.println(sql);
-
-            if (serverSession.noIndexUsed()) {
-                System.out.println("This query no index to used");
-            } else if (serverSession.noGoodIndexUsed()) {
-                System.out.println("This query has good index to used");
-            } else {
-                System.out.println("This query has bad index to used");
-            }
-
-            return null;
-        }
+        System.out.println("\n=== Track Session State ===");
+        stateChangeConn.createStatement().executeUpdate("SET NAMES utf8mb4");
     }
 }
