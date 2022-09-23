635855a6682db40ce298eaf89d698b1dd1ec16f7
lastincisor <64511824+lastincisor@users.noreply.github.com>
2022-09-23 14:10:15 +0800

    Update App.java

--- src/main/java/com/pingcap/App.java
+++ src/main/java/com/pingcap/App.java
@@ -16,37 +16,45 @@ import java.util.Properties;
 public class App 
 {
     public static void main( String[] args ) throws SQLException {
-        String dbUrl = "jdbc:mysql://localhost:4000/test?user=root&password=&useServerPrepStmts=true";
+        String dbUrl = "jdbc:mysql://172.16.5.185:4000/test?&useServerPrepStmts=true" +
+				"&sslMode=disabled&allowPublicKeyRetrieval=true" +
+				"&allowMultiQueries=false&rewriteBatchedStatements=false&useLocalTransactionState=true"+
+				"&useLocalSessionState=true";
+		final Properties props = new Properties();
+		props.put("user", "root");
+		props.put("password", "root");
+		Connection preInsertConn = DriverManager.getConnection(dbUrl,props);
+		preInsertConn.createStatement().execute("DROP TABLE IF EXISTS testBug89948");
+		preInsertConn.createStatement().execute("CREATE TABLE testBug89948 (id INT PRIMARY KEY)");
 
-        Connection preInsertConn = DriverManager.getConnection(dbUrl);
-        preInsertConn.createStatement().execute("DROP TABLE IF EXISTS testBug89948");
-        preInsertConn.createStatement().execute("CREATE TABLE testBug89948 (id INT PRIMARY KEY)");
+//		boolean allowMQ = false;
+//		boolean rwBatchStmts = true;
+//		boolean useLTS = true;
 
-        boolean allowMQ = false;
-        boolean rwBatchStmts = true;
-        boolean useLTS = true;
 
-        final Properties props = new Properties();
-        props.setProperty(PropertyKey.sslMode.getKeyName(), PropertyDefinitions.SslMode.DISABLED.name());
-        props.setProperty(PropertyKey.allowPublicKeyRetrieval.getKeyName(), "true");
+//
+//		props.setProperty(PropertyKey.USER.getKeyName(), "root");
+//		props.setProperty(PropertyKey.PASSWORD.getKeyName(), "root");
+//		props.setProperty(PropertyKey.sslMode.getKeyName(), PropertyDefinitions.SslMode.DISABLED.name());
+//		props.setProperty(PropertyKey.allowPublicKeyRetrieval.getKeyName(), "true");
+//
+//		props.setProperty(PropertyKey.allowMultiQueries.getKeyName(), Boolean.toString(allowMQ));
+//		props.setProperty(PropertyKey.rewriteBatchedStatements.getKeyName(), Boolean.toString(rwBatchStmts));
+//		props.setProperty(PropertyKey.useLocalTransactionState.getKeyName(), Boolean.toString(useLTS));
+//		props.setProperty(PropertyKey.useLocalSessionState.getKeyName(), Boolean.toString(useLTS));
 
-        props.setProperty(PropertyKey.allowMultiQueries.getKeyName(), Boolean.toString(allowMQ));
-        props.setProperty(PropertyKey.rewriteBatchedStatements.getKeyName(), Boolean.toString(rwBatchStmts));
-        props.setProperty(PropertyKey.useLocalTransactionState.getKeyName(), Boolean.toString(useLTS));
-        props.setProperty(PropertyKey.useLocalSessionState.getKeyName(), Boolean.toString(useLTS));
+		Connection testConn = DriverManager.getConnection(dbUrl, props);
+		testConn.setAutoCommit(false);
 
-        Connection testConn = DriverManager.getConnection(dbUrl, props);
-        testConn.setAutoCommit(false);
+		PreparedStatement pstmt = testConn.prepareStatement("INSERT INTO testBug89948 VALUES (?)");
+		for (int i = 1; i <= 10; i++) {
+			pstmt.setInt(1, i);
+			pstmt.addBatch();
+		}
+		pstmt.executeBatch();
+		testConn.commit();
 
-        PreparedStatement pstmt = testConn.prepareStatement("INSERT INTO testBug89948 VALUES (?)");
-        for (int i = 1; i <= 10; i++) {
-            pstmt.setInt(1, i);
-            pstmt.addBatch();
-        }
-        pstmt.executeBatch();
-        testConn.commit();
-
-        pstmt.close();
-        testConn.close();
+		pstmt.close();
+		testConn.close();
     }
 }
